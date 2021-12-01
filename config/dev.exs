09aef53b4e9f1d0772070a8da308957b37bfbd08
Josh Proehl <josh@daedalusdreams.com>
2021-12-01 14:37:25 -0600

    Run on custom local port.
    This prevents conflicts if developing multiple apps at once.
    Also 11 July, `69 was when Space Oddity was released.

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :major_tom, MajorTom.Repo,
 config :major_tom, MajorTomWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 11769],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
