3ac658c6368a51ca4a2db6c60f86b30234c4b642
Josh Proehl <josh@daedalusdreams.com>
2021-12-30 14:55:38 -0600

    Add tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -29,7 +29,15 @@ config :major_tom, MajorTomWeb.Endpoint,
   secret_key_base: "3gG2luktpUycMQ09WIB1neVEsw7gXAaGAgfewtR5rk5WDsJXhlYkQHYOe8o5Zoyj",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
